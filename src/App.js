3e7babd32f038ed94f1df55c2e0bdaa3f79c3d9a
dkrol24 <krold2191@gmail.com>
2023-04-15 22:50:00 +0200

    /

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <a href="https://d-track0.pl/p/C4kJ/MPCK/pOtw" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MILFFINDER
+              MILFFINDER program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -42,7 +42,7 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/dxn0/uiHP" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              PINEROTICA
+              PINEROTICA program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -54,7 +54,7 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/3zNg/9TIC" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              WIECEJNIZFLIRT
+              WIECEJNIZFLIRT program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -66,7 +66,7 @@ function App() {
         <a href="https://leadmy.pl/p/C4kJ/jcJX/dbm8" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              HOOKUP4LOCALS
+              HOOKUP4LOCALS program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -77,7 +77,7 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/GmZ7/5SsT" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              FOR ADULT DATING
+              FOR ADULT DATING program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -88,7 +88,7 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/1dpm/s1jL" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MAINSTREAM dating
+              MAINSTREAM dating program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -99,7 +99,7 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/Evv8/T8cN" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MAINSTREAM dating 2
+              MAINSTREAM dating 2 program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -110,7 +110,18 @@ function App() {
         <a href="https://redirecting5.eu/p/C4kJ/taBX/cQZo" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              ADULT DATING TIKTOK
+              ADULT DATING TIKTOK program
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting5.eu/p/C4kJ/1StC/A1SC" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              GAY DATING program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
