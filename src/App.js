4a59e7925bf0ba36149f57cbc42150e8f8036a5a
dkrol24 <krold2191@gmail.com>
2023-05-19 12:02:32 +0200

    .

--- src/App.js
+++ src/App.js
@@ -27,86 +27,6 @@ function App() {
         <div
           style={{ background: "black", width: "100%", height: "1px" }}
         ></div>
-        <a href="https://d-track0.pl/p/C4kJ/MPCK/pOtw" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MILFFINDER program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
-              ma w domu!
-            </a>
-          </div>
-        </a>
-        <a href="https://redirecting5.eu/p/C4kJ/dxn0/uiHP" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              PINEROTICA program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
-              ma w domu!
-            </a>
-          </div>
-        </a>
-        <a href="https://redirecting5.eu/p/C4kJ/3zNg/9TIC" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              WIECEJNIZFLIRT program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Umów się na herbatę albo od razu zerżnij napaloną siostrę
-              przyrodnią w wannie!
-            </a>
-          </div>
-        </a>
-        <a href="https://leadmy.pl/p/C4kJ/jcJX/dbm8" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              HOOKUP4LOCALS program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
-            </a>
-          </div>
-        </a>
-        <a href="https://redirecting5.eu/p/C4kJ/GmZ7/5SsT" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              FOR ADULT DATING program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
-            </a>
-          </div>
-        </a>
-        <a href="https://redirecting5.eu/p/C4kJ/1dpm/s1jL" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MAINSTREAM dating program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
-            </a>
-          </div>
-        </a>
-        <a href="https://redirecting5.eu/p/C4kJ/Evv8/T8cN" target="_blank">
-          <div className="div">
-            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              MAINSTREAM dating 2 program
-            </a>
-            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
-              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
-              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
-            </a>
-          </div>
-        </a>
         <a href="https://redirecting5.eu/p/C4kJ/taBX/cQZo" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
@@ -129,10 +49,22 @@ function App() {
             </a>
           </div>
         </a>
-        <a href="https://redirecting5.eu/p/C4kJ/Pn8j/0rBn" target="_blank">
+        <a href="https://d-track0.pl/p/C4kJ/MPCK/pOtw" target="_blank">
           <div className="div">
             <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
-              GAY DATING PROGRAM
+              MILFFINDER program
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Mama z sąsiedztwa czeka abyś u niej zagościł gdy męża nie
+              ma w domu!
+            </a>
+          </div>
+        </a>
+        <a href="https://redirecting5.eu/p/C4kJ/GmZ7/5SsT" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              FOR ADULT DATING program
             </a>
             <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
               <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
@@ -140,7 +72,17 @@ function App() {
             </a>
           </div>
         </a>
-
+        <a href="https://leadmy.pl/p/C4kJ/jcJX/dbm8" target="_blank">
+          <div className="div">
+            <a className="a1" style={{ fontSize: "25px", fontWeight: "300" }}>
+              HOOKUP4LOCALS program
+            </a>
+            <a className="a2" style={{ fontSize: "15px", fontWeight: "300" }}>
+              <span style={{ color: "black", fontSize: "20px" }}>POLECANE</span>
+              <br /> Znajdź napaloną mamuśkę w okolicy i wypieprz ją teraz!!!
+            </a>
+          </div>
+        </a>
         <div style={{ background: "black", width: "80%", height: "1px" }}></div>
         <div style={{ background: "black", width: "80%", height: "1px" }}></div>
         <div style={{ background: "black", width: "80%", height: "1px" }}></div>
