2ff1f85e8bff4aa9309a6a061b389cdf5c9fade1
dkrol24 <krold2191@gmail.com>
2023-08-30 11:34:24 +0200

    .

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import { Gallery } from "./container";
 import { Header } from "./container";
 import { Intro } from "./container";
 import { Laurels } from "./container";
-import MojaKomponenta from "./container/Banner";
+
 import "./App.css";
 
 const App = () => (
   <div>
-    <MojaKomponenta />
     <AboutUs />
     <Intro />
     <Chef />
